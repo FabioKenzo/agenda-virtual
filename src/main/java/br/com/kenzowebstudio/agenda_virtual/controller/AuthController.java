d2b61323c5c18417f98e5a34509cefcd837fd626
FabioKenzo <fabiok1999@hotmail.com>
2026-08-30 15:30:33 -0300

    feat: adiciona endpoint de login

--- src/main/java/br/com/kenzowebstudio/agenda_virtual/controller/AuthController.java
+++ src/main/java/br/com/kenzowebstudio/agenda_virtual/controller/AuthController.java
@@ -2,13 +2,20 @@ package br.com.kenzowebstudio.agenda_virtual.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.kenzowebstudio.agenda_virtual.dto.LoginRequest;
+import br.com.kenzowebstudio.agenda_virtual.dto.LoginResponse;
 import br.com.kenzowebstudio.agenda_virtual.dto.UserRegisterRequest;
 import br.com.kenzowebstudio.agenda_virtual.dto.UserRegisterResponse;
+import br.com.kenzowebstudio.agenda_virtual.model.User;
+import br.com.kenzowebstudio.agenda_virtual.security.JwtService;
 import br.com.kenzowebstudio.agenda_virtual.service.UserService;
 
 @RestController
@@ -16,19 +23,41 @@ import br.com.kenzowebstudio.agenda_virtual.service.UserService;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
-    public AuthController(UserService userService){
+    public AuthController(
+            UserService userService,
+            AuthenticationManager authenticationManager,
+            JwtService jwtService) {
+
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+
     }
 
     @PostMapping("/register")
     public ResponseEntity<UserRegisterResponse> register(
-        @RequestBody UserRegisterRequest request
-    ){
+            @RequestBody UserRegisterRequest request) {
         UserRegisterResponse response = userService.register(request);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(), request.senha()));
+
+        User user = (User) authentication.getPrincipal();
+        String token = jwtService.generateToken(user);
+
+        return ResponseEntity.ok(new LoginResponse(token));
+
     }
 }
